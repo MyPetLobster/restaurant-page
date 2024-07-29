2df5d5b89d0f1ab9fd1cced1bfb811b77a7564e6
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-07-29 18:07:06 -0400

    Setup webpack for CSS
    - update dep, add styles.css in src, import in index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,3 @@
-// After converting to ES6 module syntax
-// import { resolve, join } from 'path';
-// import HtmlWebpackPlugin from 'html-webpack-plugin';
-// export const entry = './src/index.js';
-// export const output = {
-//     filename: 'main.js',
-//     path: resolve(__dirname, 'dist'),
-// };
-// export const devServer = {
-//     contentBase: join(__dirname, 'dist'),
-//     port: 9000,
-//     open: true,
-// };
-// export const plugins = [
-//     new HtmlWebpackPlugin({
-//         template: './src/index.html',
-//         inject: 'head',
-//         scriptLoading: 'defer',
-//     }),
-// ];
-
-
-// Below is the original code from the webpack documentation before converting to ES6 module syntax
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
@@ -28,6 +5,18 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
