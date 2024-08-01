eacaa15aa608fdfc9b23c3521ab22e6b066ff431
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-07-31 23:10:19 -0400

    Add dish images and logo
    Update webpack config for image-webpack-loader
    
    Create test elem in idx.js to load images into dist
    
    Update gitignore with DS_Store

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+import shoyuRamen from './images/shoyu-ramen.jpg';
+import suzukiyaLogo from './images/ramen-suzukiya-logo.png';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const img = document.createElement('img');
+    img.src = shoyuRamen;
+    document.body.appendChild(img);
+
+    const img2 = document.createElement('img');
+    img.src = suzukiyaLogo
+    document.body.appendChild(img2);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
@@ -15,11 +17,35 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            optipng: {
+                                enabled: true,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    },
+                ],
             },
         ],
     },
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        static: path.join(__dirname, 'dist'),
         port: 9000,
         open: true,
     },
