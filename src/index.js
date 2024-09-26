f5eeb59a3304c4622ce5ab0af1b5c88838bbd8a6
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-09-26 15:36:29 -0400

    Fix nav button colors
    Add images webpack style with js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import './styles.css';
 import shoyuRamen from './images/dishes/shoyu-ramen.jpg';
 import suzukiyaLogo from './images/misc/ramen-suzukiya-logo.png';
+import githubLogo from './images/misc/github-logo.png';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const img = document.createElement('img');
-    img.src = shoyuRamen;
-    document.body.appendChild(img);
-
-    const img2 = document.createElement('img');
-    img.src = suzukiyaLogo
-    document.body.appendChild(img2);
+    const mainLogo = document.querySelector('.main-logo');
+    mainLogo.src = suzukiyaLogo;
+    const shoyuRamenElements = document.querySelectorAll('.shoyu');
+    shoyuRamenElements.forEach(element => {
+        element.src = shoyuRamen;
+    });
+    const githubLogoElement = document.querySelector('.github-icon');
+    githubLogoElement.src = githubLogo;
 });
